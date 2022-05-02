2c502396e254ea2095dd1a4e0726434657894a9f
Alexandra <yashina.aleks@gmail.com>
2022-05-02 17:08:00 +0300

    add solution for 3-module 2-task

---
+++ 03-streams/02-line-split-stream/index.js
@@ -0,0 +1,22 @@
+const LineSplitStream = require('./LineSplitStream');
+const os = require('os');
+const fs = require('fs');
+
+const lines = new LineSplitStream({
+  encoding: 'utf-8',
+});
+
+const outStream = fs.createWriteStream('out.txt');
+
+lines.pipe(outStream);
+
+function onData(line) {
+  console.log('onData');
+  console.log(line);
+}
+
+lines.on('data', onData);
+
+lines.write(`первая строка${os.EOL}вторая строка${os.EOL}третья строка${os.EOL}`);
+
+lines.end();
