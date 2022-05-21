4e8364470dfb7b30b2bca501ddeab3a91e30b1d9
Alexandra <yashina.aleks@gmail.com>
2022-05-21 20:14:54 +0300

    add solution for 5-module 1-task

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -9,9 +9,21 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/subscribe', async (ctx, next) => {
+	await new Promise(resolve => {
+		app.on('publish', (param) => {
+			resolve();
+			ctx.body = param;
+		})
+	});
+	return next();	
 });
 
 router.post('/publish', async (ctx, next) => {
+	if (ctx.request.body.message) {
+		app.emit('publish', ctx.request.body.message);
+		ctx.res.statusCode = 200;
+	}
+	return next();
 });
 
 app.use(router.routes());
