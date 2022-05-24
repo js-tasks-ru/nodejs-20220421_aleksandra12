bbf7474d712648209632af720d781203f0c35ba8
Alexandra <yashina.aleks@gmail.com>
2022-05-24 22:24:20 +0300

    add solution for 6-module 1-task

--- 06-mongodb-mongoose/01-schema-model/models/Category.js
+++ 06-mongodb-mongoose/01-schema-model/models/Category.js
@@ -2,9 +2,23 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true,
+	},
 });
 
 const categorySchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	// почему такой формат записи связи проваливает тесты? На уроке именно такой вид разбирали
+	// subcategories: [{
+	// 	type: mongoose.Schema.Types.ObjectId,
+	// 	ref: 'SubCategory',
+	// }],
+	subcategories: [subCategorySchema]
 });
 
 module.exports = connection.model('Category', categorySchema);
